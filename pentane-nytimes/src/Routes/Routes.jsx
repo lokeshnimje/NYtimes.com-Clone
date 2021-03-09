5ac9912c4c432d64a7edb4bda4488a494fc5fa59
Lokesh Nimje <lokesh.nimje@gmail.com>
2021-03-09 17:25:35 +0530

    wrapped index file with browserrouter and created routes

---
+++ pentane-nytimes/src/Routes/Routes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Route} from "react-router-dom"
+const Routes = () => {
+    return (
+        <>
+            <Route>
+                <h3>Page Not Found</h3>
+            </Route>
+        </>
+    )
+}
+
+export default Routes
