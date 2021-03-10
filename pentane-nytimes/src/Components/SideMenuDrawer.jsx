7ee9aa110baa86bd4956bc84e1f31259fde7b518
Lokesh Nimje <lokesh.nimje@gmail.com>
2021-03-10 12:29:16 +0530

    Navbar added with menu

---
+++ pentane-nytimes/src/Components/SideMenuDrawer.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import MenuIcon from '@material-ui/icons/Menu';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: '200px',
+  },
+  menu :{
+      fontSize: '10px',
+      fontWeight : "bolder",
+      padding: "2px 20px"
+  }
+});
+
+export default function SideMenuDrawer(){
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    left: false,
+  });
+  const category1 = ['Home Page', 'World', 'U.S.', 'Politics','N.Y.', 'Business','Opinion','Tech','Science','Health','Sports']
+  const category2 = ['Arts','Books','Style','Food','Travel','Magazine','T Magzine','Real Estate','Obituaries','Video','The Upshot']
+  
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+        className={classes.fullList}  onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {category1.map((text) => (
+          <ListItem className={classes.menu} button key={text}>
+            <ListItemText  primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {category2.map((text) => (
+          <ListItem className={classes.menu} button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+    return (
+        <div>
+          <React.Fragment key="left">
+          <MenuIcon onClick={toggleDrawer('left', true)}/>
+          <SwipeableDrawer
+            anchor={'left'}
+            open={state['left']}
+            onClose={toggleDrawer('left', false)}
+            onOpen={toggleDrawer('left', true)}
+          >
+            {list('left')}
+          </SwipeableDrawer>
+        </React.Fragment>
+      
+    </div>
+  );
+}
