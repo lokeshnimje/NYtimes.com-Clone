dffbade108312fa6d0d3ceb8842505b3fe2f05f7
Aejaz <aejazahmedbnm@gmail.com>
2021-03-11 15:06:11 +0530

    footer n nav together

--- pentane-nytimes/src/App.js
+++ pentane-nytimes/src/App.js
@@ -5,8 +5,8 @@ import { FooterContainer } from "./Components/FooterContainer"
 function App() {
   return (
     <div className="App">
-      <Routes/>
-    <FooterContainer />
+        <Routes/>
+        <FooterContainer/>
     </div>
   );
 }
