637b087e202724fd9bae6bc1fcc75562f1491f06
Aejaz <aejazahmedbnm@gmail.com>
2021-03-12 23:54:29 +0530

    search page added

--- pentane-nytimes/src/Redux/store.js
+++ pentane-nytimes/src/Redux/store.js
@@ -1,8 +1,9 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import { authReducer } from "./auth/reducer";
 import { getReducer } from "./getData/reducer";
+import { searchedReducer } from "./searchedData/reducer";
 import thunk from "redux-thunk";
 
-const rootReducer=combineReducers({auth:authReducer,getData:getReducer});
-
-export const store=createStore(rootReducer,applyMiddleware(thunk));
+// const rootReducer=combineReducers({auth:authReducer,getData:getReducer});
+const rootReducer=combineReducers({auth:authReducer,getData:getReducer, searchedData: searchedReducer});
+export const store= createStore(rootReducer,applyMiddleware(thunk));
